52301541d5fd63b1a04412f6e3ffd3b104f14d57
maguina-sj <jomaguina90@gmail.com>
2023-01-05 09:34:09 -0800

    practice array lists

---
+++ ArrayLists/ArrayLists.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public final class ArrayLists {
+    
+    public static void main(String[] args){
+        String[] words = {"it", "is", "this", "we", "do", "how"};
+        ArrayList<String> phrase = new ArrayList<String>();
+
+        // Pull words from the 'words' array to populate the 'phrase' array list
+        phrase.add(words[2]);
+        phrase.add(words[1]);
+        phrase.add(words[5]);
+        phrase.add(words[3]);
+        phrase.add(words[4]);
+        phrase.add(words[0]);
+        // Print the 'phrase' array list
+        // Expected output: [this, is, how, we, do, it]
+
+        System.out.println(phrase);
+    }
+
+}
